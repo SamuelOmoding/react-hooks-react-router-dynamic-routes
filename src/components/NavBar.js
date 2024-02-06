64766c0ab4185a17dc49b1816a944dc6fd471a57
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-02-06 23:16:03 +0300

    Assignment done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,12 +8,12 @@ function NavBar() {
         borderBottom: "2px solid black",
         paddingBottom: "10px",
         marginBottom: "12px",
-      }}
+        backgroundColor: "orange",   }}
     >
-      <NavLink style={{ marginRight: "10px" }} to="/">
+      <NavLink style={{ marginRight: "20px", color: "white", fontSize: "24px" }} to="/">
         Home
       </NavLink>
-      <NavLink style={{ marginRight: "10px" }} to="/movies">
+      <NavLink style={{ marginRight: "20px", color: "white", fontSize: "24px", }} to="/movies">
         Movies
       </NavLink>
     </div>
